e9980623a81ebf40754a9a7015b6234fd148a625
chan <90619921+chani017@users.noreply.github.com>
2022-04-18 00:05:46 +0900

    Add files via upload

--- jsm/webxr/ARButton.js
+++ jsm/webxr/ARButton.js
@@ -65,7 +65,7 @@ class ARButton {
 
 				currentSession.removeEventListener( 'end', onSessionEnded );
 
-				button.textContent = 'AR 시작하기';
+				button.textContent = 'START AR';
 				sessionInit.domOverlay.root.style.display = 'none';
 
 				currentSession = null;
@@ -80,7 +80,7 @@ class ARButton {
 			button.style.left = 'calc(50% - 50px)';
 			button.style.width = '100px';
 
-			button.textContent = 'START AR';
+			button.textContent = 'AR 시작하기';
 
 			button.onmouseenter = function () {
 
